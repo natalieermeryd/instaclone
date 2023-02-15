c2b6ecd89b34e80748893f19433aa9fa726a71bd
natalieermeryd <natalie.ermeryd@outlook.com>
2023-02-16 00:59:17 +0100

    hmm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Post from "./Post";
 import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -49,16 +50,18 @@ function App() {
 
   useEffect(
     () => {
-      //this is where the code runs, only once it runs
-      db.collection("posts").onSnapshot((snapshot) => {
+      const fetchPosts = async () => {
+        //this is where the code runs, only once it runs
+        const querySnapshot = await getDocs(collection(db, "posts"));
         //onSnapshot => every single time a new post is added, this code fires off
         setPosts(
-          snapshot.docs.map((doc) => ({
+          querySnapshot.docs.map((doc) => ({
             id: doc.id,
             post: doc.data(),
           }))
         );
-      });
+      };
+      fetchPosts();
     },
     [] /** Conditions goes into here [], it gonna run once when the app components loads but also every single time posts changes  */
   );
@@ -75,7 +78,7 @@ function App() {
 
       <h1>instaClone</h1>
 
-      {posts.map((id, post) => (
+      {posts.map(({ id, post }) => (
         <Post
           key={id}
           username={post.username}
@@ -83,37 +86,6 @@ function App() {
           imageUrl={post.imageUrl}
         />
       ))}
-
-      <Post
-        username="natta.es"
-        caption="it works"
-        imageUrl="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/43ccba0f-17af-4790-942a-37643349e68e/d38xqpt-d64e490b-1fc7-44a2-804d-bc683bd3d87b.jpg/v1/fill/w_800,h_758,q_75,strp/water_drop_by_othzen_d38xqpt-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQzY2NiYTBmLTE3YWYtNDc5MC05NDJhLTM3NjQzMzQ5ZTY4ZVwvZDM4eHFwdC1kNjRlNDkwYi0xZmM3LTQ0YTItODA0ZC1iYzY4M2JkM2Q4N2IuanBnIiwiaGVpZ2h0IjoiPD03NTgiLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLndhdGVybWFyayJdLCJ3bWsiOnsicGF0aCI6Ilwvd21cLzQzY2NiYTBmLTE3YWYtNDc5MC05NDJhLTM3NjQzMzQ5ZTY4ZVwvb3RoemVuLTQucG5nIiwib3BhY2l0eSI6OTUsInByb3BvcnRpb25zIjowLjQ1LCJncmF2aXR5IjoiY2VudGVyIn19.OWn5p-LZiKX7rPjnZqRDjnS3sRXHkyRzTQH-wQ_cEDQ"
-      />
-      <Post
-        username="hoorigram"
-        caption="hoorigram is the shit"
-        imageUrl="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcR4KP0sEPRfzdEXr8GXecgOu6QrPYLO0N_jz2Lyd6Ng-dsY3yWuRdwEyle5QobuLsQHzNEkfd7CCswpRUxLtO0"
-      />
-      <Post
-        username="apnangram"
-        caption="apnangram ftw"
-        imageUrl="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-      />
-      <Post
-        username="carogram"
-        caption="backendlover"
-        imageUrl="https://d3s3zh7icgjwgd.cloudfront.net/AcuCustom/Sitename/DAM/093/Tempest-Rose-Credit-House-of-Burlesque_Main.png"
-      />
-      <Post
-        username="santagram"
-        caption="spain finally"
-        imageUrl="https://sh-assets.holidu.com/imagecache/blog-photos/17596_Fill_800_800.jpg"
-      />
-      <Post
-        username="bamigram"
-        caption="hello world"
-        imageUrl="https://res.cloudinary.com/g5-assets-cld/image/upload/x_0,y_808,h_2665,w_5330,c_crop/q_auto,f_auto,fl_lossy,g_center,h_1000,w_2000/g5/g5-c-iqbdwwmq-oakland-management-multifamily-client/g5-cl-1jkj1ycnt2-the-residences-at-newcity/uploads/DS1527_4147_buj9li.jpg"
-      />
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,23 @@
+/**import firebase from "firebase";
+
+const firebaseApp = firebase.initalizeApp({
+  apiKey: "AIzaSyBHpeXpoHUzqVaitG4V8VMQA69ySGVsH2k",
+  authDomain: "natta-instaclone.firebaseapp.com",
+  databaseURL: "https://natta-instaclone-default-rtdb.firebaseio.com",
+  projectId: "natta-instaclone",
+  storageBucket: "natta-instaclone.appspot.com",
+  messagingSenderId: "357417956518",
+  appId: "1:357417956518:web:f6ed4b642d819cc0f03067",
+  measurementId: "G-4N6GZFKF25",
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export { db, auth, storage };
+*/
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
